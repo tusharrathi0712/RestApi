cc1b85d0b7f35129b8039a5defae6477202a06ea
tusharrathi0712 <tusharrathi0712@gmail.com>
2016-03-15 13:50:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,113 +2,144 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var http = require('http');
-//var routes = require('./routes/index');
-//var client= require('./routes/client');
 var app = express();
-//var router = express.Router();
+var request = require('request');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+	extended : false
+}));
 
+//GET ALL USERS
+app.get('/users', function(req, res) {
 
-
-
-
-app.get('/users', function (req, res) {
-    
 	var options = {
-		    host: 'localhost',
-		    port: 3000,
-		    path: '/users',
-		    method: 'GET',	 
+		host : 'localhost',
+		port : 3000,
+		path : '/users',
+		method : 'GET',
 	};
-    // HTTP GET request
-    var reqGet = http.request(options, function(response) {
-        response.on('data', function(data) {         	
-        	var user = JSON.parse(data);
-        	res.render('users', { data: user });                        
-        });
-    });
-    reqGet.end();    
-    reqGet.on('error', function(e) {
-        console.error(e);
-    });
+	// HTTP GET request
+	var reqGet = http.request(options, function(response) {
+		response.on('data', function(data) {
+			var user = JSON.parse(data);
+			res.render('users', {
+				data : user
+			});
+		});
+	});
+	reqGet.end();
+	reqGet.on('error', function(e) {
+		console.error(e);
+	});
 });
 
+//DELETE USER BY ID
+app.get('/users/delete/:id', function(req, res) {
 
-app.get('/users/delete/:id', function (req, res) {
-    
 	var id = req.params.id;
 	var options = {
-		    host: 'localhost',
-		    port: 3000,
-		    path: '/users/'+id,
-		    method: 'DELETE',	 
+		host : 'localhost',
+		port : 3000,
+		path : '/users/' + id,
+		method : 'DELETE',
 	};
-    // HTTP GET request
-    var reqGet = http.request(options, function(response) {
-        response.on('data', function(data) {         	
-        	//var user = JSON.parse(data);
-        	//res.render('try', { data: user });  
-        	res.redirect('/users');
-        });
-    });
-    reqGet.end();    
-    reqGet.on('error', function(e) {
-        console.error(e);
-    });
+	// HTTP GET request
+	var reqGet = http.request(options, function(response) {
+		response.on('data', function(data) {
+			// var user = JSON.parse(data);
+			// res.render('try', { data: user });
+			res.redirect('/users');
+		});
+	});
+	reqGet.end();
+	reqGet.on('error', function(e) {
+		console.error(e);
+	});
 });
 
-
-app.get('/users/add',function (req, res){
+//ADD USER PAGE
+app.get('/users/add', function(req, res) {
 	res.render('add_users');
 });
 
-
-app.post('/users/add', function (req, res) {
+//INSERTING NEW USER
+app.post('/users/add', function(req, res) {
 	var post_data = {
-	    	"firstname": req.body.firstname,
-			"lastname": req.body.lastname,
-			"email": req.body.email,
-			"id": req.body.id	
-	    };
-	console.log(post_data);
-	//console.log(post_data.firstname + " " +post_data.lastname + " " + post_data.email + " "+ post_data.id);
-	var body = JSON.stringify(post_data);
-	//var body = JSON.parse(post_data);
-	
-	var options = {
-		    host: 'localhost',
-		    port: 3000,
-		    path: '/users',
-		    method: 'POST',			
-		    headers: {
-		        "content-type": "application/json",
-		        },		  
-		    body: body
+		firstname : req.body.firstname,
+		lastname : req.body.lastname,
+		email : req.body.email,
+		id : req.body.id
 	};
-	
-	
-	
-    var reqGet = http.request(options, function(response) {
-    	
-    	response.on('data', function(data) {         	
-        	var user = JSON.parse(data);
-        	res.render('try', { data: user });
-        	//res.redirect('/users');
-        });
-    });
 
-    reqGet.on('error', function(e) {
-        console.error(e);
-    });
+	var db = JSON.stringify(post_data);
+
+	request.post({
+		headers : {
+			'content-type' : 'application/json'
+		},
+		url : 'http://localhost:3000/users',
+		body : db
+	}, function(error, response, res_body) {
+		console.log(res_body);
+		// var user = JSON.parse(res_body);
+		// res.render('try', { data: user });
+		res.redirect('/users');
+	});
 });
 
 
+//GET USER BY ID
+app.get('/users/edit/:id', function(req, res) {
+
+	var id = req.params.id;
+	var options = {
+		host : 'localhost',
+		port : 3000,
+		path : '/users/' + id,
+		method : 'GET',
+	};
+	// HTTP GET request
+	var reqGet = http.request(options, function(response) {
+		response.on('data', function(data) {
+			var user = JSON.parse(data);
+			res.render('edit_users', {
+				data : user
+			});
+		});
+	});
+	reqGet.end();
+	reqGet.on('error', function(e) {
+		console.error(e);
+	});
+});
+
+//UPDATE USER
+app.post('/users/edit', function(req, res) {
+	var post_data = {
+		firstname : req.body.firstname,
+		lastname : req.body.lastname,
+		email : req.body.email,
+		id : req.body.id
+	};
+
+	var db = JSON.stringify(post_data);
+
+	request.put({
+		headers : {
+			'content-type' : 'application/json'
+		},
+		url : 'http://localhost:3000/users',
+		body : db
+	}, function(error, response, res_body) {
+		// var user = JSON.parse(res_body);
+		// res.render('try', { data: user });
+		res.redirect('/users');
+	});
+});
 
 module.exports = app;
